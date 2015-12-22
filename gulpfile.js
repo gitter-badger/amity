cbaf78a03502ba3a2e32af993e26c60fbb3d8ff8
Luca Bianchi <luca.bianchi@neosperience.com>
2015-12-22 22:55:19 +0100

    test runner and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 "use strict";
+
 var gulp = require("gulp");
 var gutil = require("gulp-util");
+var jasmineNode = require("gulp-jasmine-node");
 
-gulp.task("default", function(){
+gulp.task('test', function() {
+    return gulp.src(["tests/**/*[S|s]pec.js"])
+        .pipe(jasmineNode({
+            timeout: 10000,
+            color: true,
+            verbose: true
+        }));
+});
+
+
+gulp.task("default", function() {
 
 });
