a19b64df32895d3fd111a5c1d31cd737976aee3b
Luca Bianchi <luca.bianchi@neosperience.com>
2016-02-02 00:54:33 +0100

    Manages globs to collect resource files from within project folder

--- src/amity.js
+++ src/amity.js
@@ -20,7 +20,8 @@ var S3Manager = require("./aws/managers/S3Manager");
 
 var CONF = {
     PROJECT_FILE: ".amity.project",
-    AWS_MANAGERS_PATH: "./aws/managers"
+    AWS_MANAGERS_PATH: "./aws/managers",
+    PATTERN_AMITY_RESOURCES: "**/*.res.json"
 };
 
 /**
@@ -40,13 +41,26 @@ var CONF = {
  * @property    resources.api       {Array.<APIEndpoint>}   Collection of APIEndpoint objects representing APIs to be created into AWS account
  */
 
+
 /**
- *
+ * Creates a new Amity object to handle serverless project configuration
+ * @param startPath     {string}        Path where project is located and amity should start work
+ * @param amityConfig   {AmityConfig}   Amity configuration object
  * @class
  */
 var Amity = function(startPath, amityConfig) {
 
-    this.config = {};
+    /** @type {AmityConfig} **/
+    this.config = {
+        resources: {
+            dynamodb: [],
+            s3: [],
+            sns: [],
+            lambda: [],
+            api: []
+        }
+    };
+
     _.extend(this, new Validator.Validatable());
     _.merge(this.config, amityConfig);
     this.config.projectPath = startPath;
@@ -63,11 +77,48 @@ var Amity = function(startPath, amityConfig) {
     };
 
     this.validateProjectConfig = function() {
-        return true;
+        return true;  //FIXME: remove this and set validation
     };
 
-    this.collectConfigFiles = function() {
+    /**
+     * Collects every file ending with *.res.json and aggregates it into
+     * @param projectFolder     {string}        Path of the folder containing project files.
+     * @param options           {object}        Value object with initialization options.
+     * @returns {Promise}       A promise that handles this task
+     */
+    this.collectConfigFiles = function(projectFolder, options) {
+        var globby = require('globby');
+        projectFolder = projectFolder || this.config.projectPath;
+        options = options || {};
+        var _self = this;
 
+        return globby(CONF.PATTERN_AMITY_RESOURCES)
+            .then(function(resourceFileName) {
+                // reads resource file into an obejct
+                return fs.readFileAsync(resourceFileName);
+            })
+            .then(function(resourceString) {
+                return JSON.parse(resourceString);
+            })
+            .then(function(resource) {
+                if (_.isArray(resource)) {
+                    // check wheter or not it is an array. If it is
+                    // iterates over every resource in the file identified by glob
+                    for (var key in resource) {
+                        if (resource.hasOwnProperty(key)) {
+                            _self.config.resources[key].concat(resource[key]);
+                        }
+
+                    }
+                } else if (_.isObject(resource)) {
+                    for (var key in resource) {
+                        if (resource.hasOwnProperty(key)) {
+                            _self.config.resources[key].push(resource[key]);
+                        }
+                    }
+                }
+                return _self.config;
+            });
     };
 
     this.setupCloud = function() {
@@ -79,6 +130,7 @@ var Amity = function(startPath, amityConfig) {
      * @param projectFolder     {string}        Path of the folder containing project files.
      * @param options           {object}        Value object with initialization options.
      * @param options.config    {AmityConfig}   Amity Configuration Object provided as parameter. It can override existing configs.
+     * @returns {Promise}       A promise mapping all folders being set up
      */
     this.init = function(projectFolder, options) {
         this.config.projectPath = projectFolder || this.config.projectPath;
